c64b6d4e20fdc09091678be5497d26e7d5e283ee
Shamik H <shamik05@hotmail.com>
2020-06-10 17:19:59 -0400

    Became frustrated and copied previous template

---
+++ script.js
@@ -0,0 +1,12 @@
+var startButton = document.querySelector("#btnStart");
+
+startButton.addEventListener("click", startQuiz);
+
+function startQuiz() {
+    startButton.classList.add("hide");
+    console.log("Start Quiz");
+}
+
+function nextQuestion() {
+
+}
