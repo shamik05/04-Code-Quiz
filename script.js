7459f949ed059e1fb3e45b9d2f67c79019c75e51
Shamik H <shamik05@hotmail.com>
2020-06-10 17:41:40 -0400

    Started working on the start button javascript

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
 var startButton = document.querySelector("#btnStart");
-
+var stopButton = document.querySelector("#btnStop");
+var highButton = document.querySelector("#btnHigh");
+var questions = [
+    {
+        question:
+    }
+]
 startButton.addEventListener("click", startQuiz);
 
 function startQuiz() {
-    startButton.classList.add("hide");
-    console.log("Start Quiz");
+    startButton.setAttribute("style", "display: none");
+    highButton.setAttribute("stye", "display: none");
+    highButton.classList.add("hide");
+    stopButton.classList.remove("hide");
+    stopButton.setAttribute("style", "margin-left: 40%;")
+    document.querySelector(".card-header").classList.remove("hide");
+    document.querySelector(".card-body").classList.remove("hide");
+    document.querySelector(".card-footer").classList.remove("hide");
+    nextQuestion()
 }
 
 function nextQuestion() {
