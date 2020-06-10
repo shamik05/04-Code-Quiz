71ad65986f4e5f03933139221f2b7c3150fc0980
Shamik H <shamik05@hotmail.com>
2020-06-10 18:01:43 -0400

    Finished questions array

--- script.js
+++ script.js
@@ -1,11 +1,7 @@
 var startButton = document.querySelector("#btnStart");
 var stopButton = document.querySelector("#btnStop");
 var highButton = document.querySelector("#btnHigh");
-var questions = [
-    {
-        question:
-    }
-]
+var questionsShuffle, questionsCurrent;
 startButton.addEventListener("click", startQuiz);
 
 function startQuiz() {
@@ -21,5 +17,63 @@ function startQuiz() {
 }
 
 function nextQuestion() {
-
+    
 }
+
+
+var questions = [
+    {
+        question: "What does CSS stand for?",
+        answers: [
+            {text: "Compact Style Sheet", correct: false},
+            {text: "Computer Screen Sheet", correct: false},
+            {text: "Complex Style Sheet", correct: false},
+            {text: "Cascading Style Sheet", correct: true}
+        ]
+    },
+    {
+        question: "Commonly used data types DO NOT include:",
+        answers: [
+            {text: "strings", correct: false},
+            {text: "booleans", correct: false},
+            {text: "alerts", correct: true},
+            {text: "numbers", correct: false}
+        ]
+    },
+    {
+        question: "The condition in an if / else statement is enclosed within ____.",
+        answers: [
+            {text: "quotes", correct: false},
+            {text: "curly brackets", correct: false},
+            {text: "parentheses", correct: true},
+            {text: "square brackets", correct: false}
+        ]
+    },
+    {
+        question: "Arrays in Javascript can be used to store ____.",
+        answers: [
+            {text: "numbers and strings", correct: false},
+            {text: "other arrays", correct: false},
+            {text: "booleans", correct: false},
+            {text: "all of the above", correct: true}
+        ]
+    },
+    {
+        question: "String values must be enclosed within ____ when being assigned to variables.",
+        answers: [
+            {text: "commas", correct: false},
+            {text: "curly brackets", correct: false},
+            {text: "quotes", correct: true},
+            {text: "paranthesis", correct: false}
+        ]
+    },
+    {
+        question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+        answers: [
+            {text: "JavaScript", correct: false},
+            {text: "terminal/bash", correct: false},
+            {text: "for loops", correct: false},
+            {text: "console log", correct: true}
+        ]
+    }
+]
